f3590a471c4cace734e2dda4fa91049f3f511e4f
KIM-JS-95 <baugh248730@gmail.com>
2021-03-02 22:58:45 +0900

    2021-03-02

---
+++ muliply.go
@@ -0,0 +1,12 @@
+package main
+
+
+import "fmt"
+
+func muliply(a int ,b int) int{
+return a*b;
+}
+
+func main(){
+fmt.Println(muliply(2,2))
+}
---
+++ package main.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func muliply(a,b){
+	return a*b;
+}
+func main() {
+
+	const name string = "nonon"
+	fmt.Println(name)
+}
