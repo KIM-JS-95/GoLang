c29bf79038d07a24ea3b62b7400666d3cd5afdd8
KIM-JS-95 <baugh248730@gmail.com>
2021-03-07 17:58:42 +0900

    2021-03-07

--- URL/main.go
+++ URL/main.go
@@ -1,42 +1,27 @@
 package main
 
 import (
-	"errors"
 	"fmt"
-	"net/http"
+	"time"
 )
 
-var errRequstFailed = errors.New("err")
-
 func main() {
-	var results = make(map[string]string)
-	urls := []string{
-		"https://www.naver.com",
-		"https://www.jajaja.com",
+	c := make(chan string) //채널 생성
+
+	people := [2]string{"a", "b"}
+	for _, person := range people {
+		go isSexy(person, c)
 	}
 
-	for _, url := range urls {
-		result := "OK"
-		err := hitURL(url)
-		if err != nil {
-			result = "FAILED"
-		}
-		results[url] = result
-	}
+	fmt.Println("Waiting for message ")
 
-	for url, result := range results {
-		fmt.Println(url, result)
+	for i := 0; i < len(people); i++ {
+		fmt.Println("Received this message ", <-c)
 	}
 
 }
 
-func hitURL(url string) error {
-	fmt.Println("Checking", url)
-	resp, err := http.Get(url)
-
-	if err != nil || resp.StatusCode >= 400 {
-		fmt.Println(err, resp.StatusCode)
-		return errRequstFailed
-	}
-	return nil
+func isSexy(person string, c chan string) {
+	time.Sleep(time.Second * 10)
+	c <- person + " is sexy"
 }
